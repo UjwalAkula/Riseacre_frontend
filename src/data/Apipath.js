661c95598f750989a2b56ac17dedd104cd14f104
Ujwal Akula <ujwalakula@gmail.com>
2025-06-29 10:38:08 +0530

    deleted unnecessary files

--- src/data/Apipath.js
+++ src/data/Apipath.js
@@ -1,3 +1,3 @@
-export  const API_URL="http://localhost:4000";
+//export  const API_URL="http://localhost:4000";
 
-//export  const API_URL="https://riseacre.onrender.com";
+export  const API_URL="https://riseacre.onrender.com";
